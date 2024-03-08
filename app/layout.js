05c55cea4a8c31ff9c5256dd3bec07775e84d8ee
fendrick <its.fendrick@example.com>
2024-03-08 19:48:22 +0530

    first commit

--- app/layout.js
+++ app/layout.js
@@ -1,4 +1,7 @@
 import { Inter } from "next/font/google";
+import Image from "next/image";
+import mainLogo from "../public/picture.png"
+import menuIcon from "../public/app.png"
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -9,9 +12,48 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
+
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+
+        <nav>
+
+          <div className="navDiv">
+            <Image src={mainLogo} width={45} />
+            <h1>Perfect Site</h1>
+          </div>
+
+
+          <div className="navDiv">
+            <ul>
+              <li>Products</li>
+              <li>Blogs</li>
+              <li id="navPricing">Pricing</li>
+              <li id="navTools">Tools</li>
+              <li id="navContact">Contact</li>
+            </ul>
+            <button>Login</button>
+            <menu> <Image src={menuIcon} width={25} />
+            <div id="menuContent">
+              <li>Home</li>
+              <li id="menuProducts">Products</li>
+              <li id="menuBlogs">Blogs</li>
+              <li id="menuPricing">Pricing</li>
+              <li id="menuTools">Tools</li>
+              <li id="menuContact">Contact</li>
+            </div>
+            </menu>
+
+          </div>
+
+
+
+        </nav>
+
+
+
+        {children}</body>
     </html>
   );
 }
